e5b133b1318fe478ad9a5544940c7b2a0e273a0a
amadordavila505 <proalexander5@gmail.com>
2023-08-26 16:32:52 -0600

    category

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { MarcasModule } from './marcas/marcas.module';
+import { FilesModule } from './files/files.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'piarou',
+      database: 'postgres',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ProductsModule,
+    UsersModule,
+    MarcasModule,
+    FilesModule,
+    
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
